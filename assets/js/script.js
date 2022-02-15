317c5c1b73a6dbbabd1b9e0a5615eb3e9329156b
cell gonzales <aragonzales.k@gmail.com>
2022-02-15 13:51:30 -0800

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,25 @@ var tasksTodoEl = document.querySelector("#tasks-to-do");
 
 //the task handler function tells the page WHAT to do after its listener is triggered
 //and must be defined first.
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
  //the event argument tells the browser to NOT do its usual behavior
     event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name='task-name'").value;
+    var taskTypeInput = document.querySelector("select[name='task-type'").value;
 
+    //creates list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is the new task.";
+    //create div to hold task info and add to list item 
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    //add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksTodoEl.appendChild(listItemEl);
 };
 
