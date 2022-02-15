2a973cc9ef253e2dd6f0dd0b14d5fb48eee33cd4
cell gonzales <aragonzales.k@gmail.com>
2022-02-15 13:18:51 -0800

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
-//console.log(buttonEl);
+//these variables link the elements from the html
+var formEl = document.querySelector("#task-form");
 var tasksTodoEl = document.querySelector("#tasks-to-do");
 
+//the task handler function tells the page WHAT to do after its listener is triggered
+//and must be defined first.
 var createTaskHandler = function(){
+ //the event argument tells the browser to NOT do its usual behavior
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is the new task.";
     tasksTodoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+//this is what the webpage is looking to see if it will happen aka the trigger.
+formEl.addEventListener("submit", createTaskHandler);
